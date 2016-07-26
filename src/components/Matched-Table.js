6773d522d76688c2cc70ab7a3f37711008d41d36
Devin Pastoor <devin.pastoor@gmail.com>
2016-07-26 15:27:03 -0400

    moved matching columns into mobx store

--- src/components/Matched-Table.js
+++ src/components/Matched-Table.js
@@ -34,57 +34,10 @@ class MatchedTable extends React.Component {
   }
 
   _getMatchedColumns() {
-    let matchedRows = this.props.tableListStore.tables.map((table, i) => {
-      return table.colDetails
-        .filter(row => row.matched)
-        .reduce((howMatched, row) => {
-          const {column, newName} = row;
-          let simpleRow = {column, newName};
-          simpleRow.tableIndex = i+1;
-          if (simpleRow.newName != "") {
-            howMatched.renamed = howMatched.renamed.concat(simpleRow);
-          } else {
-            howMatched.matched = howMatched.matched.concat(simpleRow);
-          }
-          return howMatched;
-        }, {renamed: [], matched: []})
-    })
 
-    // for now test opposite what will want
-    let renamedRows = matchedRows.map(tbl => tbl.renamed.sort((a, b) => {
-      if (a.column > b.column) {
-        return -1;
-      }
-      if (a.column < b.column) {
-        return 1
-      }
-      return 0;
-    }));
-    let renamedTable = [];
-    let renamedTableNames = ["newName"];
-    renamedRows.forEach((tbl, i) => {
-      renamedTableNames.push(`column${i+1}`);
-    })
-    renamedRows[0].forEach((row, i) => {
-      let newRow = {newName: row.newName};
-      renamedRows.forEach((tbl, tbli) => {
-        newRow[[`column${tbli+1}`]] = tbl[i].column;
-      });
-      renamedTable.push(newRow);
-    })
-    // any with matches but no newName must exist in every table
-    // any with newname must exist in each table
-    console.log("-----matchedRows-------");
-    console.log(JSON.stringify(renamedRows));
-    console.log(JSON.stringify(renamedTable));
-    return {
-      table: renamedTable,
-      colNames: renamedTableNames
-    }
   }
   render() {
-    let {tableListStore, listKeys} = this.props;
-    let {table, colNames} = this._getMatchedColumns();
+    let {table, colNames} = this.props.tableListStore.matchedColumns;
   return(
     <div style={{width: "100%", minHeight: '40vh'}}>
       <AutoSizer>
--- src/stores/TableListStore.js
+++ src/stores/TableListStore.js
@@ -52,6 +52,56 @@ export default class TableListStore {
   @action renameAllSelected(newName) {
     this.tables.forEach(tbl => tbl.renameSelectedColumn(newName))
   }
+  
+  @computed get matchedColumns() {
+    let matchedRows = this.tables.map((table, i) => {
+      return table.colDetails
+        .filter(row => row.matched)
+        .reduce((howMatched, row) => {
+          const {column, newName} = row;
+          let simpleRow = {column, newName};
+          simpleRow.tableIndex = i+1;
+          if (simpleRow.newName != "") {
+            howMatched.renamed = howMatched.renamed.concat(simpleRow);
+          } else {
+            howMatched.matched = howMatched.matched.concat(simpleRow);
+          }
+          return howMatched;
+        }, {renamed: [], matched: []})
+    });
+
+    // for now test opposite what will want
+    let renamedRows = matchedRows.map(tbl => tbl.renamed.sort((a, b) => {
+      if (a.column > b.column) {
+        return -1;
+      }
+      if (a.column < b.column) {
+        return 1
+      }
+      return 0;
+    }));
+    let renamedTable = [];
+    let renamedTableNames = ["newName"];
+    renamedRows.forEach((tbl, i) => {
+      renamedTableNames.push(`column${i+1}`);
+    })
+    renamedRows[0].forEach((row, i) => {
+      let newRow = {newName: row.newName};
+      renamedRows.forEach((tbl, tbli) => {
+        newRow[[`column${tbli+1}`]] = tbl[i].column;
+      });
+      renamedTable.push(newRow);
+    })
+    // any with matches but no newName must exist in every table
+    // any with newname must exist in each table
+    console.log("-----matchedRows-------");
+    console.log(JSON.stringify(renamedRows));
+    console.log(JSON.stringify(renamedTable));
+    return {
+      table: renamedTable,
+      colNames: renamedTableNames
+    }
+  }
   static fromJS(...array) {
     const tableListStore = new TableListStore();
     tableListStore.tables = array.map(row => TableStore.fromJS(row));
